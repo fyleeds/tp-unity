e1ceeb98249dc72dbf06e268a532f810524a537c
cpenot <clem.cmp@hotmail.com>
2024-01-08 14:35:46 +0100

    change movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,29 +3,21 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed = 1000;
-    private Rigidbody2D rb;
+    public float Speed = 5;
     private Vector2 movement;
     public float maxY, minY;
 
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
     void Update()
     {
         // Input
-        //Vector2 po = transform.position;
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement y = Mathff.Clamp(Pose.y);
+        Vector2 movement = transform.position;
+        movement += new Vector2(0, Speed) *  Input.GetAxisRaw("Vertical") * Time.deltaTime ;
+        movement.y = Mathf.Clamp(movement.y, minY, maxY);
+        transform.position = movement;
+       // movement.x = Input.GetAxisRaw("Horizontal");
+       // movement.y = Input.GetAxisRaw("Vertical");
+        
     }
 
-    void FixedUpdate()
-    {
-        // Movement
-        rb.MovePosition(rb.position + movement * Speed * Time.fixedDeltaTime);
-    }
 }
