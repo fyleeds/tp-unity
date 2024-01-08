1ea31c2be9b0768d9cbd7047b53380a4c0927916
cpenot <clem.cmp@hotmail.com>
2024-01-08 14:17:05 +0100

    design

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float Speed = 1000;
     private Rigidbody2D rb;
     private Vector2 movement;
+    public float maxY, minY;
+
 
     void Start()
     {
@@ -14,13 +17,15 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // Input
+        //Vector2 po = transform.position;
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        movement y = Mathff.Clamp(Pose.y);
     }
 
     void FixedUpdate()
     {
         // Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * Speed * Time.fixedDeltaTime);
     }
 }
