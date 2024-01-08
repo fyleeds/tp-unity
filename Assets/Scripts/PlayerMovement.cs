3027d3151656c2a28fc76b3ab0d976643fa2e59e
cpenot <clem.cmp@hotmail.com>
2024-01-08 14:53:55 +0100

    remake movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,29 @@ using UnityEngine.UIElements;
 public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 5;
-    private Vector2 movement;
     public float maxY, minY;
+    private Rigidbody2D rb;
+    private Vector2 movement;
 
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
         // Input
-        Vector2 movement = transform.position;
-        movement += new Vector2(0, Speed) *  Input.GetAxisRaw("Vertical") * Time.deltaTime ;
+        //
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
         movement.y = Mathf.Clamp(movement.y, minY, maxY);
-        transform.position = movement;
-       // movement.x = Input.GetAxisRaw("Horizontal");
-       // movement.y = Input.GetAxisRaw("Vertical");
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * Speed * Time.fixedDeltaTime);
     }
 
 }
